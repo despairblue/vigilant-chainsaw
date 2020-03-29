7476efc73f1c37ca31763499fc4d221c7b05f566
Danny Martini <danny@wunderflats.com>
2020-03-29 11:47:06 +0200

    hook up play button in menu

---
+++ vigilant-chainsaw/Assets/PlayButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public Button playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(Play);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene("Game");
+    }
+}
