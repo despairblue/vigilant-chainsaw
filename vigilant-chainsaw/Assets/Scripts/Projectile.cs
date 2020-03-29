2888fb7b11fac5e27d291550fa362ed142376d2a
Danny Martini <danny@wunderflats.com>
2020-03-29 10:54:42 +0200

    projectiles self destruct

--- vigilant-chainsaw/Assets/Scripts/Projectile.cs
+++ vigilant-chainsaw/Assets/Scripts/Projectile.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float selfDestructThreshold = -10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < selfDestructThreshold)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.transform.CompareTag("Person"))
+        {
+            GameObject.Destroy(gameObject);
+        }
     }
 }
