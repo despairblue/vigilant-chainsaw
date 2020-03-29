bb39e726e683ec7a41711bd4dbe252dd10a044ca
Danny Martini <danny@wunderflats.com>
2020-03-29 15:33:43 +0200

    hook up back button

---
+++ vigilant-chainsaw/Assets/CreditScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditScreen : MonoBehaviour
+{
+    public Button backButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton.onClick.AddListener(Back);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
