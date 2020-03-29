320b7064c095f0b90d92d5848ed6dc5276f05e64
Danny Martini <danny@wunderflats.com>
2020-03-29 11:35:00 +0200

    hook up lifes

--- vigilant-chainsaw/Assets/GameState.cs
+++ vigilant-chainsaw/Assets/GameState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameState : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI highScoreHUD;
+    public List<GameObject> lifes;
     public int currentScore = 0;
     public int currentLifes = 3;
 
@@ -23,17 +24,35 @@ public class GameState : MonoBehaviour
     private void Render()
     {
         highScoreHUD.SetText(currentScore.ToString());
+
+        for (int i = 0; i < lifes.Count; i++)
+        {
+            var go = lifes[i];
+            var emptyHeart = go.transform.Find("Empty");
+            var fullHeart = go.transform.Find("Full");
+
+            if (i < currentLifes)
+            {
+                emptyHeart.gameObject.SetActive(false);
+                fullHeart.gameObject.SetActive(true);
+            }
+            else
+            {
+                emptyHeart.gameObject.SetActive(true);
+                fullHeart.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void IncreaseScore(int delta)
     {
         currentScore += delta;
-
         Render();
     }
 
     public void ReduceLife()
     {
         currentLifes -= 1;
+        Render();
     }
 }
